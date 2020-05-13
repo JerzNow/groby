1c32f64669e75853836e752238f3c0e525bac7a0
Aleksander Nowinski <aleksander.nowinski@gmail.com>
2020-05-13 20:23:55 +0200

    Minor updates for file format

--- data_extraction/wikidata/fetch_wikidata.bash
+++ data_extraction/wikidata/fetch_wikidata.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -x
+#set -x
 for sparql in *.sparql;
 do
 	echo "Processing file $sparql"
 	query=$(cat $sparql)
 	output=`basename $sparql .sparql`.csv
-        curl -H "Accept: text/csv" -o "$output" -v --data-urlencode "query=$query" https://query.wikidata.org/sparql
+        curl -H "Accept: text/csv" -o "$output" --data-urlencode "query=$query" https://query.wikidata.org/sparql
 	line_count=$(wc -l $output)
 	echo "Completed, output $output has $line_count lines"
 	sleep 60
